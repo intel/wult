14180f307b774e0abc14e3a106e8a716127248f4
Antti Laakso <antti.laakso@linux.intel.com>
2020-11-18 10:13:31 +0200

    setup.py: silence pylint
    Change-Id: I33c6294ac84766f66b66b85794a14923e420e999
    Signed-off-by: Antti Laakso <antti.laakso@linux.intel.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@
 
 """The standard python packaging script."""
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="wult-web",
@@ -18,11 +18,11 @@ setup(
     author_email="artem.bityutskiy@linux.intel.com",
     install_requires=["pelican", "ghp-import", "typogrify"],
     long_description="""Dummy package to bring pelican dependencies.""",
-	classifiers=[
-		"Intended Audience :: Developers",
+    classifiers=[
+        "Intended Audience :: Developers",
         "Intended Audience :: Science/Research"
         "Topic :: System :: Hardware",
-		"Topic :: System :: Operating System Kernels :: Linux",
-		"License :: OSI Approved :: BSD License",
-	],
+        "Topic :: System :: Operating System Kernels :: Linux",
+        "License :: OSI Approved :: BSD License",
+    ],
 )
