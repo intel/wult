6ff2a2e4dd78926eab3d70622bf3ab73b9dd0f32
Artem Bityutskiy <dedekind1@gmail.com>
2020-12-16 15:36:09 +0200

    wult: add statistics collection support
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

---
+++ wultlibs/WultStatsCollect.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+# vim: ts=4 sw=4 tw=100 et ai si
+#
+# Copyright (C) 2019-2020 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Author: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
+
+"""
+This module is just a "glue" layer between "WultRunner" and "StatsCollect".
+"""
+
+import logging
+import contextlib
+from wultlibs.helperlibs.Exceptions import Error
+from wultlibs.statscollectlibs import StatsCollect, StatsHelpers
+
+_LOG = logging.getLogger()
+
+STATS_NAMES = list(StatsCollect.DEFAULT_STINFO)
+STATS_INFO = StatsCollect.DEFAULT_STINFO
+
+def parse_stats(stnames, intervals):
+    """Parse user-provided lists of statistics and intervals."""
+
+    stconf = StatsHelpers.parse_stnames(stnames)
+    if intervals:
+        StatsHelpers.parse_intervals(intervals, stconf)
+
+    return stconf
+
+class WultStatsCollect:
+    """
+    The statistics collector class. Built on top of 'StatsCollect', but simplifies the API a little
+    bit for wult usage scenario.
+    """
+
+    def start(self):
+        """Start collecting statistics."""
+
+        self._stcoll.start()
+
+    def stop(self):
+        """Stop collecting statistics."""
+
+        self._stcoll.stop()
+
+    def apply_stconf(self, stconf):
+        """Configure the statistics according to the 'stconf' dictionary contents."""
+
+        if stconf["discover"] or "acpower" in stconf["include"]:
+            # Assume that power meter is configured to match the SUT name.
+            if self._proc.is_remote:
+                devnode = self._proc.hostname
+            else:
+                devnode = "default"
+            self._stcoll.set_prop("acpower", "devnode", devnode)
+
+        StatsHelpers.apply_stconf(self._stcoll, stconf)
+        self._stcoll.configure()
+
+    def copy_stats(self):
+        """Copy collected statistics and statistics log from remote SUT to the local system."""
+
+        if not self._proc.is_remote:
+            return
+
+        _, routdir = self._stcoll.get_outdirs()
+        if not routdir:
+            # No in-band statistics were collected, so nothing to copy.
+            return
+
+        _LOG.debug("copy in-band statistics from '%s:%s' to '%s'",
+                   self._proc.hostname, routdir, self._outdir)
+
+        # We add trailing slash to the remote directory path in order to make rsync copy the
+        # contents of the remote directory, but not the directory itself.
+        self._proc.rsync(f"{routdir}/", self._outdir, remotesrc=True, remotedst=False)
+
+    def __init__(self, proc, res):
+        """
+        The class constructor. The arguments are as follows.
+          * proc - the 'Proc' or 'SSH' object that defines the host to collect the statistics about.
+          * res - the 'WORawResult' object to store the results at.
+          """
+
+        self._proc = proc
+        self._outdir = res.dirpath
+        self._stcoll = None
+
+        # Create the local statistics collector data and log directories.
+        try:
+            path = self._outdir
+            path.mkdir(exist_ok=True)
+        except OSError as err:
+            raise Error(f"failed to create directory '{path}': {err}")
+
+        self._stcoll = StatsCollect.StatsCollect(proc, local_outdir=self._outdir.resolve())
+
+    def close(self):
+        """Close the statistics collector."""
+
+        if getattr(self, "_stcoll", None):
+            with contextlib.suppress(Error):
+                self._stcoll.close()
+            self._stcoll = None
+
+        if getattr(self, "_proc", None):
+            self._proc = None
+
+    def __del__(self):
+        """The destructor."""
+        self.close()
+
+    def __enter__(self):
+        """Enter the run-time context."""
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Exit the runtime context."""
+        self.close()
