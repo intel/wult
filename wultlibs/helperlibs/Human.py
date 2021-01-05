a334f6e1ae128c6415d81375940a1bff394ae77f
Artem Bityutskiy <dedekind1@gmail.com>
2021-01-05 15:39:50 +0200

    helperlibs: Human: add 'parse_duration()' helper
    Add a helper function for parsing user-defined time durations, like '1h10m10s',
    which is 1 hour, 10 minutes, 10 seconds.
    
    Change-Id: I6caa8fbb40b774d9701620d965984b706da5a351
    Signed-off-by: Artem Bityutskiy <dedekind1@gmail.com>

--- wultlibs/helperlibs/Human.py
+++ wultlibs/helperlibs/Human.py
@@ -11,6 +11,9 @@ This module contains misc. helper functions with the common theme of representin
 human-readable format, or turning human-oriented data into a machine format.
 """
 
+from wultlibs.helperlibs import Trivial
+from wultlibs.helperlibs.Exceptions import Error
+
 def duration(seconds, s=True, ms=False):
     """
     Transform duration in seconds to the human-readable format. The 's' and 'ms' arguments control
@@ -42,3 +45,52 @@ def duration(seconds, s=True, ms=False):
             result += "%ds " % secs
 
     return result.strip()
+
+# The specifiers that 'parse_duration()' accepts.
+DURATION_SPECS = {"d" : "days", "h" : "hours", "m" : "minutes", "s" : "seconds"}
+# A short 'parse_duration()' specifiers description string.
+DURATION_SPECS_DESCR = ", ".join([f"{spec} - {key}" for spec, key in DURATION_SPECS.items()])
+
+def parse_duration(htime, default_unit="s"):
+    """
+    This function does the opposite to what 'duration()' does - parses the human time string and
+    returns integer number of seconds. This function supports the following specifiers:
+      * d - days
+      * h - hours
+      * m - minutes
+      * s - seconds.
+
+    If 'htime' is just a number without a specifier, it is assumed to be in seconds. But the
+    'default_unit' argument can be used to specify a different default unit.
+    """
+
+    if default_unit not in DURATION_SPECS:
+        raise Error(f"bad unit '{default_unit}', supported units are: {DURATION_SPECS_DESCR}")
+
+    htime = htime.strip()
+    if htime.isdigit():
+        htime += default_unit
+
+    tokens = {}
+    rest = htime.lower()
+    for spec in DURATION_SPECS:
+        split = rest.split(spec, 1)
+        if len(split) > 1:
+            tokens[spec] = split[0]
+            rest = split[1]
+        else:
+            rest = split[0]
+
+    if rest.strip():
+        raise Error(f"failed to parse duration '{htime}'")
+
+    for spec, val in tokens.items():
+        if not Trivial.is_int(val):
+            raise Error(f"failed to parse duration '{htime}': bad amount of "
+                        f"{DURATION_SPECS[spec]}")
+
+    days = int(tokens.get("d", 0))
+    hours = int(tokens.get("h", 0))
+    mins = int(tokens.get("m", 0))
+    secs = int(tokens.get("s", 0))
+    return days * 24 * 60 * 60 + hours * 60 * 60 + mins * 60 + secs
