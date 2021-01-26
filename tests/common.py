65fd677151cba6e261244d8364bcb910364bda77
Antti Laakso <antti.laakso@linux.intel.com>
2021-01-26 15:36:49 +0200

    tests: move reusable code to own module
    Adding new test module next and reusing common
    functionality.
    
    Change-Id: I85f12e93389324f03183e373d5b7eaef12d5f0ef
    Signed-off-by: Antti Laakso <antti.laakso@linux.intel.com>

---
+++ tests/common.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+#
+# -*- coding: utf-8 -*-
+# vim: ts=4 sw=4 tw=100 et ai si
+#
+# Copyright (C) 2021 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Author: Antti Laakso <antti.laakso@linux.intel.com>
+
+"""Common code for test modules."""
+
+import os
+import subprocess
+from pathlib import Path
+import pytest
+from wultlibs.helperlibs import Exceptions
+
+class CmdLineRunner():
+    """Class for running commandline commands."""
+
+    @staticmethod
+    def _command(cmd):
+        """Run 'cmd' command and return output, if any."""
+
+        try:
+            result = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as err:
+            raise Exceptions.Error(str(err))
+
+        if not result:
+            return None
+
+        return result.decode("utf-8").strip()
+
+    def _has_outdir(self, cmd):
+        """Check if 'cmd' command has 'outdir' -option."""
+
+        cmd = [self._tool_path, cmd, "-h"]
+        helptext = self._command(cmd)
+
+        return "outdir" in helptext
+
+    def command(self, cmd, arg=None):
+        """Run commandline tool with arguments."""
+
+        if self._has_outdir(cmd) and self._tmpdir:
+            arg += f" -o {self._tmpdir}"
+
+        cmd = [self._tool_path, cmd]
+        if arg:
+            cmd += arg.split()
+
+        return self._command(cmd)
+
+    def __init__(self, toolname, devid, tmpdir=None):
+        """The constructor."""
+
+        self._tmpdir = str(tmpdir)
+        self._devid = devid
+
+        tooldir = Path(__file__).parents[1].resolve()
+        testdataroot = tooldir / "tests" / "testdata"
+        self._tool_path = tooldir / toolname
+        assert self._tool_path.exists()
+
+        self.good_paths = []
+        self.bad_paths = []
+        for dirpath, dirnames, _ in os.walk(testdataroot / toolname):
+            if dirnames:
+                continue
+            if "good" in Path(dirpath).parts:
+                self.good_paths.append(dirpath)
+            else:
+                self.bad_paths.append(dirpath)
+
+        assert self.good_paths
+        assert self.bad_paths
+
+class WultTest(CmdLineRunner):
+    """Class for running tests for 'wult'."""
+
+    def __init__(self, devid, tmpdir=None):
+        """The constructor."""
+
+        super().__init__("wult", devid, tmpdir)
+
+class NdlTest(CmdLineRunner):
+    """Class for running tests for 'ndl'."""
+
+    def __init__(self, devid, tmpdir=None):
+        """The constructor."""
+
+        super().__init__("ndl", devid, tmpdir)
+
+@pytest.fixture(params=[WultTest, NdlTest], ids=["wult", "ndl"])
+def tool(request, devid, tmp_path_factory):
+    """Common fixture to return test object for 'wult' and 'ndl'."""
+
+    tmpdir = tmp_path_factory.mktemp(request.fixturename)
+    return request.param(devid, tmpdir=tmpdir)
