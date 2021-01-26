e03a80cafc483ba5d645e02c7ebcf2a2cf64365e
Antti Laakso <antti.laakso@linux.intel.com>
2021-01-26 15:37:43 +0200

    tests: add tests for good input data
    Test 'filter', 'report' and 'calc' commands against
    good input data for both 'wult' and 'ndl' tools.
    
    Change-Id: I14aad6254804237e6da09f1b3743b031e8836905
    Signed-off-by: Antti Laakso <antti.laakso@linux.intel.com>

---
+++ tests/test_good_input.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#
+# -*- coding: utf-8 -*-
+# vim: ts=4 sw=4 tw=100 et ai si
+#
+# Copyright (C) 2021 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Author: Antti Laakso <antti.laakso@linux.intel.com>
+
+"""
+Test module for 'wult' project. Tests following commands for 'wult' and 'ndl':
+- filter
+- report
+- calc
+"""
+
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-import
+
+from common import tool
+
+def test_good_input_data(tool):
+    """Test 'report', 'calc', and 'start' commands for good input data."""
+
+    for cmd in ("filter", "report", "calc"):
+        for args in tool.good_paths:
+            if cmd == "filter":
+                args = f"--rfilt index!=0 {args}"
+            tool.command(cmd, args)
