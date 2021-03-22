d040a066f6a1ec189059377ebdf441251bd53ef7
Antti Laakso <antti.laakso@linux.intel.com>
2021-03-22 20:17:23 +0200

    PowerCtl: move related MSR definitions
    To keep all power control code in single module,
    move also all related MSR definitions to PowerCtl
    module.
    
    Change-Id: Iaa541e763b0fc96ee07b9a4b08982b28193dcef8
    Signed-off-by: Antti Laakso <antti.laakso@linux.intel.com>

--- wultlibs/sysconfiglibs/PowerCtl.py
+++ wultlibs/sysconfiglibs/PowerCtl.py
@@ -15,6 +15,11 @@ from wultlibs.helperlibs import Procs
 from wultlibs.sysconfiglibs import CPUInfo, MSR
 from wultlibs.helperlibs.Exceptions import ErrorNotSupported
 
+# Power control Model Specific Register.
+MSR_POWER_CTL = 0x1FC
+C1E_ENABLE = 1
+CSTATE_PREWAKE_DISABLE = 30
+
 class PowerCtl:
     """
     This class provides API for managing settings in MSR 0x1FC (MSR_POWER_CTL). This is a
@@ -29,37 +34,37 @@ class PowerCtl:
         """
 
         if bitval:
-            self._msr.set(MSR.MSR_POWER_CTL, MSR.bit_mask(bitnr), cpus=cpus)
+            self._msr.set(MSR_POWER_CTL, MSR.bit_mask(bitnr), cpus=cpus)
         else:
-            self._msr.clear(MSR.MSR_POWER_CTL, MSR.bit_mask(bitnr), cpus=cpus)
+            self._msr.clear(MSR_POWER_CTL, MSR.bit_mask(bitnr), cpus=cpus)
 
     def c1e_autopromote_enabled(self, cpu):
         """
         Returns 'True' if C1E autopromotion is enabled for CPU 'cpu', otherwise returns 'False'.
         """
 
-        regval = self._msr.read(MSR.MSR_POWER_CTL, cpu=cpu)
-        return MSR.is_bit_set(MSR.C1E_ENABLE, regval)
+        regval = self._msr.read(MSR_POWER_CTL, cpu=cpu)
+        return MSR.is_bit_set(C1E_ENABLE, regval)
 
     def set_c1e_autopromote(self, enable: bool, cpus="all"):
         """
         Enable or disable C1E autopromote for CPUs 'cpus'. The 'cpus' argument is the same as in
         '_toggle_bit()'.
         """
-        self._toggle_bit(MSR.C1E_ENABLE, int(enable), cpus)
+        self._toggle_bit(C1E_ENABLE, int(enable), cpus)
 
     def cstate_prewake_enabled(self, cpu):
         """Returns 'True' if C-state prewake is enabled for CPU 'cpu', otherwise returns 'False'."""
 
-        regval = self._msr.read(MSR.MSR_POWER_CTL, cpu=cpu)
-        return not MSR.is_bit_set(MSR.CSTATE_PREWAKE_DISABLE, regval)
+        regval = self._msr.read(MSR_POWER_CTL, cpu=cpu)
+        return not MSR.is_bit_set(CSTATE_PREWAKE_DISABLE, regval)
 
     def set_cstate_prewake(self, enable: bool, cpus="all"):
         """
         Enable or disable C-state prewake for CPUs 'cpus'. The 'cpus' argument is the same as in
         '_toggle_bit()'.
         """
-        self._toggle_bit(MSR.CSTATE_PREWAKE_DISABLE, int(not enable), cpus)
+        self._toggle_bit(CSTATE_PREWAKE_DISABLE, int(not enable), cpus)
 
     def __init__(self, proc=None, lscpuinfo=None):
         """
@@ -80,9 +85,9 @@ class PowerCtl:
 
         if self._lscpuinfo["vendor"] != "GenuineIntel":
             raise ErrorNotSupported(f"unsupported CPU model '{self._lscpuinfo['vendor']}', "
-                                    f"model-specific register {hex(MSR.MSR_POWER_CTL)} "
-                                    f"(MSR_POWER_CTL) is not available{self._proc.hostmsg}. "
-                                    f"MSR_POWER_CTL is available only on Intel platforms")
+                                    f"model-specific register {hex(MSR_POWER_CTL)} (MSR_POWER_CTL) "
+                                    f"is not available{self._proc.hostmsg}. MSR_POWER_CTL is "
+                                    f"available only on Intel platforms")
 
     def close(self):
         """Uninitialize the class object."""
