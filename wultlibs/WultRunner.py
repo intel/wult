b43e90b98759d0e40120179428cc7a902a99529c
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2021-08-18 11:36:39 +0300

    WultRunner: improve 'dump_dp()'
    Improve the datapoint dump function and make it print more compact output. With
    this patch, it prints in a table form with 3 columns. Everything is aligned and
    easy to read. Example:
    
    SilentTime:  398515             TIntr:     364149180712872    CC6Cyc: 0
    WakeLatency: 415                ReqCState: 1                  CC7Cyc: 0
    IntrLatency: 845                TotCyc:    1474256            PC2Cyc: 0
    LDist:       2778745            CC0Cyc:    42072              PC3Cyc: 0
    LTime:       364149180710220    SMICnt:    0                  PC6Cyc: 0
    TBI:         364149179753784    NMICnt:    0                  PC7Cyc: 0
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- wultlibs/WultRunner.py
+++ wultlibs/WultRunner.py
@@ -28,7 +28,38 @@ _MAX_FTRACE_BAD_LINES = 10
 def _dump_dp(dp):
     """Returns a string for datapoint 'dp' suitable for using in error messages."""
 
-    return "\n".join(f"{key}: {val}" for key, val in dp.items())
+    import numpy                      # pylint: disable=import-outside-toplevel
+    from itertools import zip_longest # pylint: disable=import-outside-toplevel
+
+    # Printing one element per line takes too many lines and it is hard to read. So we attempt to
+    # print 3 elements per line and align them for readability. So split items on 3 groups, each
+    # group represents a column. The easy way of doing this is using 'numpy.array_split()'.
+    split = [list(column) for column in numpy.array_split(numpy.array(list(dp)), 3)]
+
+    columns = []
+    for keys in split:
+        # Create list of values for the keys in the column. Shorten the floating point numbers.
+        vals = []
+        for key in keys:
+            val = dp[key]
+            if isinstance(val, float):
+                vals.append(f"{val:.2f}")
+            else:
+                vals.append(f"{val}")
+
+        longest_key = max([len(key) for key in keys])
+        longest_val = max([len(val) for val in vals])
+
+        elts = []
+        for key, val in zip(keys, vals):
+            # Pad the keys/values to the longer key/value in the column.
+            key = f"{(key + ':').ljust(longest_key + 1)}"
+            val = f"{val.ljust(longest_val)}"
+            elts.append(f"{key} {val}")
+
+        columns.append(elts)
+
+    return "\n".join(["    ".join(row).strip() for row in zip_longest(*columns, fillvalue="")])
 
 class WultRunner:
     """Run wake latency measurement experiments."""
