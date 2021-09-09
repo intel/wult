46aa19c758894e408019ee16efdef39730802251
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2021-09-09 14:44:14 +0300

    wult: driver: allocate wult_info
    Allocate the 'wult_info' objects at initialization, instead of using a static
    declaration. There is no particular reason for this right now, but will be
    helpful soon.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- drivers/idle/wult/main.c
+++ drivers/idle/wult/main.c
@@ -15,6 +15,7 @@
 #include <linux/mutex.h>
 #include <linux/processor.h>
 #include <linux/random.h>
+#include <linux/slab.h>
 #include <linux/smp.h>
 #include <linux/spinlock.h>
 #include <asm/div64.h>
@@ -35,10 +36,10 @@
 static unsigned int cpunum;
 
 /* The wult driver information object. */
-static struct wult_info wi;
+static struct wult_info *wi;
 
 /*
- * This mutex protect 'wi.pdev' and serializes delayed event driver
+ * This mutex protect 'wi->pdev' and serializes delayed event driver
  * registration and removal.
  */
 static DEFINE_MUTEX(wi_mutex);
@@ -48,35 +49,35 @@ int wult_enable(void)
 {
 	int err = 0;
 
-	spin_lock(&wi.enable_lock);
-	if (wi.enabled)
+	spin_lock(&wi->enable_lock);
+	if (wi->enabled)
 		goto out_unlock;
 
-	err = wult_tracer_enable(&wi);
+	err = wult_tracer_enable(wi);
 	if (err) {
 		wult_err("failed to enable the tracer, error %d", err);
 		goto out_unlock;
 	}
 
-	wi.enabled = true;
-	atomic_set(&wi.events_armed, 0);
-	atomic_set(&wi.events_happened, 0);
-	wake_up(&wi.armer_wq);
+	wi->enabled = true;
+	atomic_set(&wi->events_armed, 0);
+	atomic_set(&wi->events_happened, 0);
+	wake_up(&wi->armer_wq);
 
 out_unlock:
-	spin_unlock(&wi.enable_lock);
+	spin_unlock(&wi->enable_lock);
 	return err;
 }
 
 /* Disable the measurements. */
 void wult_disable(void)
 {
-	spin_lock(&wi.enable_lock);
-	if (wi.enabled) {
-		wi.enabled = false;
-		wult_tracer_disable(&wi);
+	spin_lock(&wi->enable_lock);
+	if (wi->enabled) {
+		wi->enabled = false;
+		wult_tracer_disable(wi);
 	}
-	spin_unlock(&wi.enable_lock);
+	spin_unlock(&wi->enable_lock);
 }
 
 /* Enable/disable interrupt latency focused measurements. */
@@ -84,16 +85,16 @@ int wult_set_intr_focus(bool intr_focus)
 {
 	int err = 0;
 
-	spin_lock(&wi.enable_lock);
-	if (wi.intr_focus == intr_focus || !wi.enabled)
-		wi.intr_focus = intr_focus;
+	spin_lock(&wi->enable_lock);
+	if (wi->intr_focus == intr_focus || !wi->enabled)
+		wi->intr_focus = intr_focus;
 	else
 		/*
 		 * The measurements must be disabled in order to toggle the
 		 * interrupt focus mode.
 		 */
 		err = -EINVAL;
-	spin_unlock(&wi.enable_lock);
+	spin_unlock(&wi->enable_lock);
 
 	return err;
 }
@@ -104,10 +105,10 @@ int wult_set_intr_focus(bool intr_focus)
  */
 void wult_interrupt(u64 cyc)
 {
-	wult_tracer_interrupt(&wi, cyc);
-	WRITE_ONCE(wi.event_cpu, smp_processor_id());
-	atomic_inc(&wi.events_happened);
-	wake_up(&wi.armer_wq);
+	wult_tracer_interrupt(wi, cyc);
+	WRITE_ONCE(wi->event_cpu, smp_processor_id());
+	atomic_inc(&wi->events_happened);
+	wake_up(&wi->armer_wq);
 }
 EXPORT_SYMBOL_GPL(wult_interrupt);
 
@@ -116,8 +117,8 @@ static u64 pick_ldist(void)
 {
 	u64 ldist_from, ldist_to, ldist;
 
-	ldist_from = atomic64_read(&wi.ldist_from);
-	ldist_to = atomic64_read(&wi.ldist_to);
+	ldist_from = atomic64_read(&wi->ldist_from);
+	ldist_to = atomic64_read(&wi->ldist_to);
 	if (ldist_from > ldist_to)
 		ldist_from = ldist_to;
 
@@ -127,10 +128,10 @@ static u64 pick_ldist(void)
 	ldist += ldist_from;
 
 	/* Ensure the ldist_gran. */
-	if (wi.wdi->ldist_gran > 1) {
-		ldist += wi.wdi->ldist_gran - 1;
-		do_div(ldist, wi.wdi->ldist_gran);
-		ldist *= wi.wdi->ldist_gran;
+	if (wi->wdi->ldist_gran > 1) {
+		ldist += wi->wdi->ldist_gran - 1;
+		do_div(ldist, wi->wdi->ldist_gran);
+		ldist *= wi->wdi->ldist_gran;
 	}
 
 	return ldist;
@@ -153,7 +154,7 @@ static int delayed_event_device_init(struct wult_device_info *wdi,
 		wult_err("device '%s' lauch distance resolution is %u ns, wich is too coarse, max is %d ns",
 			 wdi->devname, wdi->ldist_gran,
 			 WULT_MAX_LDIST_GRANULARITY);
-		wi.wdi->ops->exit(wi.wdi);
+		wi->wdi->ops->exit(wi->wdi);
 		return -EINVAL;
 	}
 
@@ -163,9 +164,9 @@ static int delayed_event_device_init(struct wult_device_info *wdi,
 /* Check if the armer threads runs on the correct CPU. */
 static inline int check_armer_cpunum(void)
 {
-	if (smp_processor_id() != wi.cpunum) {
+	if (smp_processor_id() != wi->cpunum) {
 		wult_err("armer thread runs on CPU%u instead of CPU%u",
-			 smp_processor_id(), wi.cpunum);
+			 smp_processor_id(), wi->cpunum);
 		return -EINVAL;
 	}
 	return 0;
@@ -189,17 +190,17 @@ static int armer_kthread(void *data)
 		goto init_error;
 
 	/* Initialize the delayed event driver. */
-	err = delayed_event_device_init(wi.wdi, wi.cpunum);
+	err = delayed_event_device_init(wi->wdi, wi->cpunum);
 	if (err)
 		goto init_error;
 
 	/* Indicate that the initialization is complete. */
-	wi.initialized = true;
-	wake_up(&wi.armer_wq);
+	wi->initialized = true;
+	wake_up(&wi->armer_wq);
 
 	while (!kthread_should_stop()) {
 		/* Sleep until we are enabled or asked to exit. */
-		wait_event(wi.armer_wq, wi.enabled || kthread_should_stop());
+		wait_event(wi->armer_wq, wi->enabled || kthread_should_stop());
 
 		if (kthread_should_stop())
 			break;
@@ -208,36 +209,36 @@ static int armer_kthread(void *data)
 		if (err)
 			goto error;
 
-		events_happened = atomic_read(&wi.events_happened);
+		events_happened = atomic_read(&wi->events_happened);
 
 		ldist = pick_ldist();
-		err = wult_tracer_arm_event(&wi, &ldist);
+		err = wult_tracer_arm_event(wi, &ldist);
 		if (err)
 			goto error;
 
-		atomic_inc(&wi.events_armed);
+		atomic_inc(&wi->events_armed);
 
 		timeout = ktime_to_ms(ns_to_ktime(ldist)) + 1000;
-		err = wait_event_timeout(wi.armer_wq,
-			 atomic_read(&wi.events_happened) != events_happened,
+		err = wait_event_timeout(wi->armer_wq,
+			 atomic_read(&wi->events_happened) != events_happened,
 			 msecs_to_jiffies(timeout));
-		if (err == 0 && wi.enabled) {
+		if (err == 0 && wi->enabled) {
 			wult_err("delayed event timed out, waited %ums", timeout);
 			goto error;
 		}
 
 		/* Check that the interrupt happend on the right CPU. */
-		event_cpu = READ_ONCE(wi.event_cpu);
-		if (event_cpu != wi.cpunum) {
+		event_cpu = READ_ONCE(wi->event_cpu);
+		if (event_cpu != wi->cpunum) {
 			if (wrong_cpu_cnt++ < 128)
 				continue;
 			wult_err("delayed event happened on CPU%u instead of CPU%u %u times, stop measuring",
-				 event_cpu, wi.cpunum, wrong_cpu_cnt);
+				 event_cpu, wi->cpunum, wrong_cpu_cnt);
 			goto error;
 		}
 
-		events_happened = atomic_read(&wi.events_happened);
-		events_armed = atomic_read(&wi.events_armed);
+		events_happened = atomic_read(&wi->events_happened);
+		events_armed = atomic_read(&wi->events_armed);
 		if (events_armed != events_happened) {
 			wult_err("events count mismatch: armed %u, got %u",
 				 events_armed, events_happened);
@@ -245,20 +246,20 @@ static int armer_kthread(void *data)
 		}
 
 		/* Send the last measurement data to user-space. */
-		spin_lock(&wi.enable_lock);
-		if (wi.enabled) {
-			err = wult_tracer_send_data(&wi);
+		spin_lock(&wi->enable_lock);
+		if (wi->enabled) {
+			err = wult_tracer_send_data(wi);
 			if (err) {
-				spin_unlock(&wi.enable_lock);
+				spin_unlock(&wi->enable_lock);
 				wult_err("failed to send data out, error %d", err);
 				goto error;
 			}
 		}
-		spin_unlock(&wi.enable_lock);
+		spin_unlock(&wi->enable_lock);
 	}
 
 	wult_dbg("exiting");
-	wi.wdi->ops->exit(wi.wdi);
+	wi->wdi->ops->exit(wi->wdi);
 	return 0;
 
 error:
@@ -270,27 +271,27 @@ error:
 		schedule();
 	}
 
-	wi.wdi->ops->exit(wi.wdi);
+	wi->wdi->ops->exit(wi->wdi);
 	return -EINVAL;
 
 init_error:
-	wi.initialized = true;
-	wi.init_err = err;
-	wake_up(&wi.armer_wq);
+	wi->initialized = true;
+	wi->init_err = err;
+	wake_up(&wi->armer_wq);
 	return err;
 }
 
 /* Initialize wult device information object 'wdi'. */
 static void init_wdi(struct wult_device_info *wdi)
 {
-	memset(&wi, 0, sizeof(struct wult_info));
-	wi.wdi = wdi;
-	wdi->priv = &wi;
-	wi.cpunum = cpunum;
-	atomic64_set(&wi.ldist_from, max(wdi->ldist_min, DEFAULT_LDIST_FROM));
-	atomic64_set(&wi.ldist_to, min(wdi->ldist_max, DEFAULT_LDIST_TO));
-	spin_lock_init(&wi.enable_lock);
-	init_waitqueue_head(&wi.armer_wq);
+	memset(wi, 0, sizeof(struct wult_info));
+	wi->wdi = wdi;
+	wdi->priv = wi;
+	wi->cpunum = cpunum;
+	atomic64_set(&wi->ldist_from, max(wdi->ldist_min, DEFAULT_LDIST_FROM));
+	atomic64_set(&wi->ldist_to, min(wdi->ldist_max, DEFAULT_LDIST_TO));
+	spin_lock_init(&wi->enable_lock);
+	init_waitqueue_head(&wi->armer_wq);
 	/*
 	 * Claculate 'mult' and 'shift' that will further be used for converting
 	 * count of cycles to nanoseconds.
@@ -311,38 +312,38 @@ int wult_register(struct wult_device_info *wdi)
 		return -ENODEV;
 
 	mutex_lock(&wi_mutex);
-	if (wi.wdi) {
-		wult_err("already have device '%s' registered", wi.wdi->devname);
+	if (wi->wdi) {
+		wult_err("already have device '%s' registered", wi->wdi->devname);
 		goto err_put;
 	}
 
 	init_wdi(wdi);
 
-	err = wult_tracer_init(&wi);
+	err = wult_tracer_init(wi);
 	if (err) {
 		wult_err("failed to initialize the tracer, error %d", err);
 		goto err_put;
 	}
 
-	wi.armer = kthread_create(armer_kthread, &wi, WULT_KTHREAD_NAME);
-	if (IS_ERR(wi.armer)) {
-		err = PTR_ERR(wi.armer);
+	wi->armer = kthread_create(armer_kthread, wi, WULT_KTHREAD_NAME);
+	if (IS_ERR(wi->armer)) {
+		err = PTR_ERR(wi->armer);
 		wult_err("failed to create the '%s' kernel thread, error %d",
 			 WULT_KTHREAD_NAME, err);
 		goto err_tracer;
 	}
 
-	kthread_bind(wi.armer, wi.cpunum);
-	wake_up_process(wi.armer);
+	kthread_bind(wi->armer, wi->cpunum);
+	wake_up_process(wi->armer);
 
 	/* Wait for the delayed event driver to finish initialization. */
-	wait_event(wi.armer_wq, wi.initialized);
-	if (wi.init_err) {
-		err = wi.init_err;
+	wait_event(wi->armer_wq, wi->initialized);
+	if (wi->init_err) {
+		err = wi->init_err;
 		goto err_tracer;
 	}
 
-	err = wult_uapi_device_register(&wi);
+	err = wult_uapi_device_register(wi);
 	if (err) {
 		wult_err("failed to create debugfs files, error %d", err);
 		goto err_kthread;
@@ -354,9 +355,9 @@ int wult_register(struct wult_device_info *wdi)
 	return 0;
 
 err_kthread:
-	kthread_stop(wi.armer);
+	kthread_stop(wi->armer);
 err_tracer:
-	wult_tracer_exit(&wi);
+	wult_tracer_exit(wi);
 err_put:
 	mutex_unlock(&wi_mutex);
 	module_put(THIS_MODULE);
@@ -367,15 +368,15 @@ EXPORT_SYMBOL_GPL(wult_register);
 /* Unregister the delayed event source. */
 void wult_unregister(void)
 {
-	wult_msg("unregistering device '%s'", wi.wdi->devname);
+	wult_msg("unregistering device '%s'", wi->wdi->devname);
 
-	wult_uapi_device_unregister(&wi);
+	wult_uapi_device_unregister(wi);
 	wult_disable();
-	kthread_stop(wi.armer);
-	wult_tracer_exit(&wi);
+	kthread_stop(wi->armer);
+	wult_tracer_exit(wi);
 
 	mutex_lock(&wi_mutex);
-	wi.wdi = NULL;
+	wi->wdi = NULL;
 	mutex_unlock(&wi_mutex);
 
 	module_put(THIS_MODULE);
@@ -408,7 +409,8 @@ static int __init wult_init(void)
 		return -EINVAL;
 	}
 
-	wi.cpunum = cpunum;
+	wi = kzalloc(sizeof(struct wult_info), GFP_KERNEL);
+	wi->cpunum = cpunum;
 
 	return 0;
 }
@@ -417,6 +419,7 @@ module_init(wult_init);
 /* Module exit function. */
 static void __exit wult_exit(void)
 {
+	kfree(wi);
 }
 module_exit(wult_exit);
 
