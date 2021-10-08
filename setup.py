a9ec3a0048e0af0c88ab1293294933cccfb6bdb5
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2021-10-08 10:50:16 +0300

    setup: add pepc dependency
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- setup.py
+++ setup.py
@@ -77,7 +77,8 @@ setup(
                get_data_files("share/wult/css", "css"),
     scripts=_TOOLNAMES + _PYTHON_HELPERS,
     packages=find_packages(),
-    install_requires=["plotly>=4", "jinja2", "numpy", "pandas", "paramiko", "pyyml", "colorama"],
+    install_requires=["pepc>=1.1.0", "plotly>=4", "jinja2", "numpy", "pandas", "paramiko", "pyyml",
+                      "colorama"],
     long_description="""This package provides wult - a Linux command-line tool for measuring Intel
                         CPU C-state wake latency.""",
     classifiers=[
