c9e40980f1998503d7e16bcd8aedeba1e3c8dd10
Adam Hawley <adam.james.hawley@intel.com>
2021-12-07 14:16:40 +0200

    js: add javascript packaging files
    To track javascript dependencies and configure javascript bundling, this
    patch adds boilerplate code to enable future javascript development.
    
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

---
+++ js/src/index.js
@@ -0,0 +1,9 @@
+/*
+ * -*- coding: utf-8 -*-
+ * vim: ts=4 sw=4 tw=100 et ai si
+ *
+ * Copyright (C) 2019-2021 Intel, Inc.
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Author: Adam Hawley <adam.james.hawley@intel.com>
+ */
---
+++ js/webpack.config.js
@@ -0,0 +1,18 @@
+/*
+ * -*- coding: utf-8 -*-
+ * vim: ts=4 sw=4 tw=100 et ai si
+ *
+ * Copyright (C) 2019-2021 Intel, Inc.
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Author: Adam Hawley <adam.james.hawley@intel.com>
+ */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
