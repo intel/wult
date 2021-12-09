40a1c719d0d821632af784460e65d9649d60c744
Adam Hawley <adam.james.hawley@intel.com>
2021-12-09 15:58:57 +0200

    js: set webpack configuration to production
    By setting this configuration to 'production', webpack will use a
    standard set of optimization options which helps us reduce the size of
    the bundled javascript.
    
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

--- html/js/webpack.config.js
+++ html/js/webpack.config.js
@@ -11,6 +11,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'production',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
