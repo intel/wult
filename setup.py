cf7677b00cebf41c92d4b7f70608eea6a5b43a5d
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2021-12-15 14:13:53 +0200

    Remove paramiko dependency
    We do not use paramiko directly. We use 'SSH' module from pepc project, and
    paramiko brouhgt by 'pepc'.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- setup.py
+++ setup.py
@@ -79,8 +79,8 @@ setup(
                get_data_files("share/wult/html/js/dist", "html/js/dist"),
     scripts=_TOOLNAMES + _PYTHON_HELPERS,
     packages=find_packages(),
-    install_requires=["pepc>=1.2.0,<1.3.0", "plotly>=4", "jinja2", "numpy", "pandas", "paramiko",
-                      "pyyml", "colorama"],
+    install_requires=["pepc>=1.2.0,<1.3.0", "plotly>=4", "jinja2", "numpy", "pandas", "pyyml",
+                      "colorama"],
     long_description="""This package provides wult - a Linux command-line tool for measuring Intel
                         CPU C-state wake latency.""",
     classifiers=[
