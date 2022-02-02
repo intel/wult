ff390e5a46502b296416d378c89d032c7d64748f
Adam Hawley <adam.james.hawley@intel.com>
2022-02-02 12:22:15 +0200

    js: add shoelace.style
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

--- html/js/src/index.js
+++ html/js/src/index.js
@@ -14,3 +14,8 @@ import { WultMetricTab } from './wult-metric-tab'
 import { IntroTable } from './intro-tbl'
 import { StatsTab } from './stats-tab'
 import '../css/style.css'
+import '@shoelace-style/shoelace/dist/themes/light.css'
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js'
+
+// Set the base path to the folder you copied Shoelace's assets to
+setBasePath('shoelace')
