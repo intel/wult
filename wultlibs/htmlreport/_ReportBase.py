d66b64e2e5cd0ea9813932fb56291d4d57776354
Adam Hawley <adam.james.hawley@intel.com>
2022-04-06 12:32:46 +0300

    _IPMITab: rename '_IPMITab.py' to '_IPMITabBuilder.py'
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

--- wultlibs/htmlreport/_ReportBase.py
+++ wultlibs/htmlreport/_ReportBase.py
@@ -20,7 +20,7 @@ from pepclibs.helperlibs import Trivial, FSHelpers
 from pepclibs.helperlibs.Exceptions import Error, ErrorNotFound
 from wultlibs import Deploy
 from wultlibs.htmlreport.tabs import _MetricTabBuilder, _Tabs
-from wultlibs.htmlreport.tabs.stats import _ACPowerTabBuilder, _IPMITab
+from wultlibs.htmlreport.tabs.stats import _ACPowerTabBuilder, _IPMITabBuilder
 
 _LOG = logging.getLogger()
 
@@ -221,7 +221,7 @@ class ReportBase:
 
         tab_builders = [
             _ACPowerTabBuilder.ACPowerTabBuilder,
-            _IPMITab.IPMITabBuilder
+            _IPMITabBuilder.IPMITabBuilder
         ]
 
         tabs = []
