68e11012b928da16797bfc4ae95d908183757dd0
Adam Hawley <adam.james.hawley@intel.com>
2022-04-15 12:11:34 +0300

    _TurbostatL2TabBuilderBase: switch to dynamic tab generation
    In future patches, the turbostat statistics tab will become more
    complicated with data tabs at varying levels in the tab hierarchy.
    
    This patch prepares for that change by changing the way turbostat L2
    tabs are built so that the tab layout can be customised and defined
    using a dictionary.
    
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

--- wultlibs/htmlreport/tabs/stats/turbostat/_TurbostatL2TabBuilderBase.py
+++ wultlibs/htmlreport/tabs/stats/turbostat/_TurbostatL2TabBuilderBase.py
@@ -14,12 +14,16 @@ For each level 2 turbostat tab, we parse raw turbostat statistics files differen
 base class expects child classes to implement '_turbostat_to_df()'.
 """
 
+from pathlib import Path
+import logging
 import pandas
 from pepclibs.helperlibs.Exceptions import Error
 from wultlibs.htmlreport.tabs.stats import _TabBuilderBase, _DTabBuilder
 from wultlibs.htmlreport.tabs import _Tabs
 from wultlibs.parsers import TurbostatParser
 from wultlibs import _DefsBase
+
+_LOG = logging.getLogger()
 
 class TurbostatL2TabBuilderBase(_TabBuilderBase.TabBuilderBase):
     """
@@ -61,6 +65,61 @@ class TurbostatL2TabBuilderBase(_TabBuilderBase.TabBuilderBase):
 
         return sdf
 
+    def _build_ctab(self, name, tab_hierarchy, outdir, defs):
+        """
+        This is a helper function for 'get_tab()'. Build a container tab according to the
+        'tab_hierarchy' dictionary. If no sub-tabs can be generated then returns 'None'.
+        Arguments are as follows:
+         * name - name of the returned container tab.
+         * tab_hierarchy - dictionary representation of the desired tab hierarchy. Schema is as
+                           follows:
+                           {
+                               CTabName1:
+                                   {"dtabs": [metric1, metric2...]},
+                               CTabName2:
+                                   CTabName3:
+                                       {"dtabs": [metric3, metric4...]}
+                           }
+         * outdir - path of the directory in which to store the generated tabs.
+         * defs - '_DefsBase.DefsBase' instance which contains all of the definitions for the
+                  metrics which appear in 'tab_hierarchy'.
+        """
+
+        # Sub-tabs which will be contained by the returned container tab.
+        sub_tabs = []
+
+        # Start by checking if 'tab_hierarchy' includes data tabs at this level. If it does, create
+        # them and append them to 'sub_tabs'.
+        if "dtabs" in tab_hierarchy:
+            for metric in tab_hierarchy["dtabs"]:
+                try:
+                    tab = _DTabBuilder.DTabBuilder(self._reports, outdir, self._basedir,
+                                                   defs.info[metric], defs.info[self._time_metric])
+                    sub_tabs.append(tab.get_tab())
+                except Error as err:
+                    _LOG.info("Skipping '%s' tab in turbostat '%s' tab: error occured during tab "
+                              "generation.", metric, self.name)
+                    _LOG.debug(err)
+
+        # Process the rest of the tabs in the tab hierarchy.
+        for tab_name, sub_hierarchy in tab_hierarchy.items():
+            # Data tabs are handled by the check above so skip them.
+            if tab_name == "dtabs":
+                continue
+
+            # Tabs not labelled by the "dtabs" key in the tab hierarchy are container tabs. For each
+            # sub container tab, recursively call 'self._build_ctab()'.
+            subdir = Path(outdir) / _DefsBase.get_fsname(tab_name)
+            subtab = self._build_ctab(tab_name, sub_hierarchy, subdir, defs)
+            if subtab:
+                sub_tabs.append(subtab)
+
+        if sub_tabs:
+            return _Tabs.CTabDC(name, sub_tabs)
+
+        # If no sub tabs were generated then return 'None'.
+        return None
+
     def get_tab(self):
         """
         Returns a '_Tabs.CTabDC' instance containing tabs which represent different metrics within
@@ -68,15 +127,17 @@ class TurbostatL2TabBuilderBase(_TabBuilderBase.TabBuilderBase):
         """
 
         defs = _DefsBase.CSDefsBase("turbostat")
-        child_tabs = []
-        for metric in self._metrics:
-            mdefs = defs.info[metric]
-            dtab = _DTabBuilder.DTabBuilder(self._reports, self.outdir / mdefs["fsname"],
-                                            self._basedir, mdefs, defs.info[self._time_metric])
-            child_tabs.append(dtab.get_tab())
 
-        return _Tabs.CTabDC(self.name, child_tabs)
+        # Find metrics which are common to all raw turbostat statistic files.
+        metric_sets = [set(sdf.columns) for sdf in self._reports.values()]
+        common_metrics = set.intersection(*metric_sets)
 
+        # Limit metrics to only those which are common to all test results.
+        for reportid, sdf in self._reports.items():
+            self._reports[reportid] = sdf[list(common_metrics)]
+
+        # Build L2 CTab with hierarchy represented in 'self._tab_hierarchy'.
+        return self._build_ctab(self.name, self._tab_hierarchy, self.outdir, defs)
 
     def __init__(self, stats_paths, outdir, basedir):
         """
@@ -94,6 +155,9 @@ class TurbostatL2TabBuilderBase(_TabBuilderBase.TabBuilderBase):
         self._metrics = {
             "CC0%": "Busy%"
         }
+
+        # Add data tabs for all the metrics in 'self._metrics' to the tab hierarchy.
+        self._tab_hierarchy = {"dtabs": list(self._metrics)}
 
         super().__init__(stats_paths, outdir, ["turbostat.raw.txt"])
         self._basedir = basedir
