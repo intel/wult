e70190f2e81677cebabcf712bb383d50e5ee2ab1
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2022-05-04 07:42:42 +0300

    TurbostatDefs: move 'populate_cstates()'
    Now only turbostat code uses 'populate_cstates()', so move it to
    'TurbostatDefs'. But the plan is to get rid of it soon.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- wultlibs/TurbostatDefs.py
+++ wultlibs/TurbostatDefs.py
@@ -15,6 +15,41 @@ from wultlibs import _DefsBase
 class TurbostatDefs(_DefsBase.DefsBase):
     """This module provides API to turbostat metrics definitions (AKA 'defs')."""
 
+    def populate_cstates(self, csnames):
+        """
+        Definitions YAML files do not contain information about C-states supported by various
+        platforms, they only use a generic C-state "Cx".  Depending on the platform, there may be a
+        different amount of C-states with different names.
+
+        This method should be invoked to populate the definitions dictionary with the C-state
+        information for a specific platform. The 'csnames' argument is the list of C-state names to
+        replace the "Cx" metric with.
+        """
+
+        pattern = "Cx"
+
+        # Copy all keys one-by-one to preserve the order.
+        info = {}
+        for metric, minfo in self.info.items():
+            if pattern not in metric:
+                info[metric] = minfo
+                continue
+
+            for csname in csnames:
+                new_metric = metric.replace(pattern, csname)
+
+                if new_metric in info:
+                    continue
+
+                info[new_metric] = minfo.copy()
+
+                # Correct all info dicts which need populating to refer to real C-state names.
+                for key in self._populate_cstate_keys:
+                    if key in info[new_metric]:
+                        info[new_metric][key] = info[new_metric][key].replace(pattern, csname)
+
+        self.info = info
+
     def __init__(self):
         """The class constructor."""
 
