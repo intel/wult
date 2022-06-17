423b3cf5e3a7a9a8f1711370af6cb65e54916a03
Adam Hawley <adam.james.hawley@intel.com>
2022-06-17 16:39:56 +0300

    _TurbostatL2TabBuilderBase: move '_build_ctab()' to '_TabBuilderBase'
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

--- wultlibs/htmlreport/tabs/stats/_TabBuilderBase.py
+++ wultlibs/htmlreport/tabs/stats/_TabBuilderBase.py
@@ -12,12 +12,11 @@ This module provides a base class and common logic for populating a group of sta
 
 from pathlib import Path
 import logging
-
 from pepclibs.helperlibs.Exceptions import Error, ErrorNotFound
 from wultlibs import _DefsBase
+from wultlibs.htmlreport.tabs import _DTabBuilder, _Tabs
 
 _LOG = logging.getLogger()
-
 
 class TabBuilderBase:
     """
@@ -37,6 +36,77 @@ class TabBuilderBase:
 
     # The name of the statistics represented in the produced tab.
     name = None
+
+    def _build_ctab(self, name, tab_hierarchy, outdir, plots):
+        """
+        This is a helper function for 'get_tab()'. Build a container tab according to the
+        'tab_hierarchy' dictionary. If no sub-tabs can be generated then returns 'None'.
+        Arguments are as follows:
+         * name - name of the returned container tab.
+         * tab_hierarchy - dictionary representation of the desired tab hierarchy. Schema is as
+                           follows:
+                           {
+                               CTabName1:
+                                   {"dtabs": [metric1, metric2...]},
+                               CTabName2:
+                                   CTabName3:
+                                       {"dtabs": [metric3, metric4...]}
+                           }
+         * outdir - path of the directory in which to store the generated tabs.
+         * plots - dictionary representation of the plots to include for each metric. Schema is as
+                   follows:
+                   {
+                        Metric1:
+                            {
+                                "scatter": [(mdef1, mdef2), (mdef1, mdef5)],
+                                "hist": [mdef1, mdef2],
+                                "chist": [mdef1]
+                            }
+                   }
+        """
+
+        # Sub-tabs which will be contained by the returned container tab.
+        sub_tabs = []
+
+        # Start by checking if 'tab_hierarchy' includes data tabs at this level. If it does, create
+        # them and append them to 'sub_tabs'.
+        if "dtabs" in tab_hierarchy:
+            for metric in tab_hierarchy["dtabs"]:
+                if not all(metric in sdf for sdf in self._reports.values()):
+                    _LOG.info("Skipping '%s' tab in '%s' tab: one or more results do not contain "
+                              "data for this metric.", metric, self.name)
+                    continue
+                try:
+                    tab = _DTabBuilder.DTabBuilder(self._reports, outdir, self._defs.info[metric],
+                                                   self._basedir)
+                    if metric in plots:
+                        tab.add_plots(plots[metric].get("scatter"), plots[metric].get("hist"),
+                                      plots[metric].get("chist"))
+                    tab.add_smrytbl([self._defs.info[metric]])
+                    sub_tabs.append(tab.get_tab())
+                except Error as err:
+                    _LOG.info("Skipping '%s' tab in '%s' tab: error occured during tab generation.",
+                              metric, self.name)
+                    _LOG.debug(err)
+
+        # Process the rest of the tabs in the tab hierarchy.
+        for tab_name, sub_hierarchy in tab_hierarchy.items():
+            # Data tabs are handled by the check above so skip them.
+            if tab_name == "dtabs":
+                continue
+
+            # Tabs not labelled by the "dtabs" key in the tab hierarchy are container tabs. For each
+            # sub container tab, recursively call 'self._build_ctab()'.
+            subdir = Path(outdir) / _DefsBase.get_fsname(tab_name)
+            subtab = self._build_ctab(tab_name, sub_hierarchy, subdir, plots)
+            if subtab:
+                sub_tabs.append(subtab)
+
+        if sub_tabs:
+            return _Tabs.CTabDC(name, sub_tabs)
+
+        # If no sub tabs were generated then return 'None'.
+        return None
 
     def get_tab(self):
         """
--- wultlibs/htmlreport/tabs/stats/turbostat/_TurbostatL2TabBuilderBase.py
+++ wultlibs/htmlreport/tabs/stats/turbostat/_TurbostatL2TabBuilderBase.py
@@ -14,16 +14,11 @@ For each level 2 turbostat tab, we parse raw turbostat statistics files differen
 base class expects child classes to implement '_turbostat_to_df()'.
 """
 
-from pathlib import Path
-import logging
 import pandas
 from pepclibs.helperlibs.Exceptions import Error
 from wultlibs.htmlreport.tabs.stats import _TabBuilderBase
-from wultlibs.htmlreport.tabs import _DTabBuilder, _Tabs
 from wultlibs.parsers import TurbostatParser
-from wultlibs import _DefsBase, TurbostatDefs
-
-_LOG = logging.getLogger()
+from wultlibs import TurbostatDefs
 
 class TurbostatL2TabBuilderBase(_TabBuilderBase.TabBuilderBase):
     """
@@ -103,77 +98,6 @@ class TurbostatL2TabBuilderBase(_TabBuilderBase.TabBuilderBase):
         sdf[self._time_metric] = sdf[self._time_metric] - sdf[self._time_metric][0]
 
         return sdf
-
-    def _build_ctab(self, name, tab_hierarchy, outdir, plots):
-        """
-        This is a helper function for 'get_tab()'. Build a container tab according to the
-        'tab_hierarchy' dictionary. If no sub-tabs can be generated then returns 'None'.
-        Arguments are as follows:
-         * name - name of the returned container tab.
-         * tab_hierarchy - dictionary representation of the desired tab hierarchy. Schema is as
-                           follows:
-                           {
-                               CTabName1:
-                                   {"dtabs": [metric1, metric2...]},
-                               CTabName2:
-                                   CTabName3:
-                                       {"dtabs": [metric3, metric4...]}
-                           }
-         * outdir - path of the directory in which to store the generated tabs.
-         * plots - dictionary representation of the plots to include for each metric. Schema is as
-                   follows:
-                   {
-                        Metric1:
-                            {
-                                "scatter": [(mdef1, mdef2), (mdef1, mdef5)],
-                                "hist": [mdef1, mdef2],
-                                "chist": [mdef1]
-                            }
-                   }
-        """
-
-        # Sub-tabs which will be contained by the returned container tab.
-        sub_tabs = []
-
-        # Start by checking if 'tab_hierarchy' includes data tabs at this level. If it does, create
-        # them and append them to 'sub_tabs'.
-        if "dtabs" in tab_hierarchy:
-            for metric in tab_hierarchy["dtabs"]:
-                if not all(metric in sdf for sdf in self._reports.values()):
-                    _LOG.info("Skipping '%s' tab in '%s' tab: one or more results do not contain "
-                              "data for this metric.", metric, self.name)
-                    continue
-                try:
-                    tab = _DTabBuilder.DTabBuilder(self._reports, outdir, self._defs.info[metric],
-                                                   self._basedir)
-                    if metric in plots:
-                        tab.add_plots(plots[metric].get("scatter"), plots[metric].get("hist"),
-                                      plots[metric].get("chist"))
-                    tab.add_smrytbl([self._defs.info[metric]])
-                    sub_tabs.append(tab.get_tab())
-                except Error as err:
-                    _LOG.info("Skipping '%s' tab in '%s' tab: error occured during tab generation.",
-                              metric, self.name)
-                    _LOG.debug(err)
-
-        # Process the rest of the tabs in the tab hierarchy.
-        for tab_name, sub_hierarchy in tab_hierarchy.items():
-            # Data tabs are handled by the check above so skip them.
-            if tab_name == "dtabs":
-                continue
-
-            # Tabs not labelled by the "dtabs" key in the tab hierarchy are container tabs. For each
-            # sub container tab, recursively call 'self._build_ctab()'.
-            subdir = Path(outdir) / _DefsBase.get_fsname(tab_name)
-            subtab = self._build_ctab(tab_name, sub_hierarchy, subdir, plots)
-            if subtab:
-                sub_tabs.append(subtab)
-
-        if sub_tabs:
-            return _Tabs.CTabDC(name, sub_tabs)
-
-        # If no sub tabs were generated then return 'None'.
-        return None
 
     @staticmethod
     def _get_common_elements(lsts):
