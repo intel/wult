63137d4472114d431416cec0728496a2a535c65a
Adam Hawley <adam.james.hawley@intel.com>
2022-06-30 17:05:59 +0300

    NdlReport: define summary functions to calculate for ndl metrics
    This patch defines the summary functions to include in ndl reports in
    'NdlReport' so that in a future patch, the hard-coded regex can be
    removed from 'ReportBase'.
    
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

--- wultlibs/htmlreport/NdlReport.py
+++ wultlibs/htmlreport/NdlReport.py
@@ -16,6 +16,14 @@ from wultlibs.htmlreport import _ReportBase
 DEFAULT_XAXES = "LDist"
 DEFAULT_YAXES = DEFAULT_HIST = DEFAULT_CHIST = "RTD"
 
+# Defines which summary functions should be calculated and included in the report for each metric.
+# Metrics are represented by their name or a regular expression and paired with a list of summary
+# functions.
+SMRY_FUNCS = {
+    "LDist": ["max", "min"],
+    "RTD": ["max", "99.999%", "99.99%", "99.9%", "99%", "med", "avg", "min", "std"]
+}
+
 class NdlReport(_ReportBase.ReportBase):
     """This module provides API for generating HTML reports for ndl test results."""
 
@@ -30,4 +38,5 @@ class NdlReport(_ReportBase.ReportBase):
                 args[name] = default.split(",")
 
         super().__init__(rsts, outdir, title_descr=title_descr, xaxes=args["xaxes"],
-                         yaxes=args["yaxes"], hist=args["hist"], chist=args["chist"])
+                         yaxes=args["yaxes"], hist=args["hist"], chist=args["chist"],
+                         smry_funcs=SMRY_FUNCS)
