intel/wult

commit 20e296f4e4a76c24c3467b3470dd13a7d43defc6

WultRunner: improve max. latency print
We print max. latency while runnint the measurements. Today, we print
'WakeLatency'. However, when running C1 measurements on platforms like IceLake
Xeon, which enters C1 with interupts enabled, the more accurate and
"comparable" latency in 'IntrLatency'. We should print it instead, and this is
what the patch does.

Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

wultlibs/WultRunner.py
@@ -65,7 +65,13 @@ class WultRunner(ClassHelpers.SimpleCloseContext):
                     # The data point has not been added (e.g., because it did not pass row filters).
                     continue
 
-                max_latency = max(dp["WakeLatency"], max_latency)
+                # Interrupt latency and wake latency are measured one after another, and the order
+                # depends on C-state interrupt order. Whatever is measured first is more accurate,
+                # because of the measurement overhead. Let's use the smaller value for calculating
+                # the max. latency that we print, assuming it was measured first and it is more
+                # accurate and trustworthy.
+                latency = min(dp["WakeLatency"], dp["IntrLatency"])
+                max_latency = max(latency, max_latency)
                 self._progress.update(collected_cnt, max_latency)
                 last_rawdp_time = time.time()
 

commit 3adfcb6fb69e368b97055d9248bedabd17375fe6

WultRunner: fix crash related to absent 'IntrLatency'
This is a regression introduced in commit

20e296f WultRunner: improve max. latency print

In some cases we do not have 'IntrLatency'. So check for it before using it.

Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

wultlibs/WultRunner.py
@@ -70,7 +70,8 @@ class WultRunner(ClassHelpers.SimpleCloseContext):
                 # because of the measurement overhead. Let's use the smaller value for calculating
                 # the max. latency that we print, assuming it was measured first and it is more
                 # accurate and trustworthy.
-                latency = min(dp["WakeLatency"], dp["IntrLatency"])
+                intr_latency = dp.get("IntrLatency", 0)
+                latency = min(dp["WakeLatency"], intr_latency)
                 max_latency = max(latency, max_latency)
                 self._progress.update(collected_cnt, max_latency)
                 last_rawdp_time = time.time()