5b20ab01044a84dd7b65768c7c6855c7fc6390d2
Adam Hawley <adam.james.hawley@intel.com>
2022-10-21 18:58:30 +0300

    js: tab: remove complicated 'visibility' logic
    This logic is made redundant by properly using web components built-in
    visibility logic. This patch removes the logic in preparation for
    removing the 'tab' parent class.
    
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

--- js/src/tab.js
+++ js/src/tab.js
@@ -8,8 +8,7 @@
  * Author: Adam Hawley <adam.james.hawley@intel.com>
  */
 
-import { LitElement, html } from 'lit'
-import { cache } from 'lit/directives/cache.js'
+import { LitElement } from 'lit'
 
 /**
  * Generic class which handles visiblity of report tabs. When creating a new tab, it is recommended
@@ -20,62 +19,6 @@ import { cache } from 'lit/directives/cache.js'
  * @extends {LitElement}
  */
 export class ScTab extends LitElement {
-    static properties = {
-        tabname: { type: String },
-        info: { type: Object },
-        visible: { type: Boolean, attribute: false }
-    };
-
-    /**
-     * Checks mutations to the parent tab element to see if the 'active' attribute has been set and
-     * sets the 'visible' attribute accordingly.
-     */
-    checkVisible (mutationsList, observer) {
-        for (const mutation of mutationsList) {
-            if (mutation.attributeName === 'active') {
-                if (this.tabname === mutation.target.id) {
-                    this.visible = true
-                } else {
-                    this.visible = false
-                }
-            }
-        }
-    }
-
-    /**
-     * Early DOM lifecycle event. Invoked each time the custom element is appended into a
-     * document-connected element.
-     */
-    connectedCallback () {
-        super.connectedCallback()
-        // Adds event listener so that the tab will re-evaulate 'visible' every time the user clicks
-        // to see if the tab has been opened. Read relevant docs here:
-        // https://lit.dev/docs/components/events/#adding-event-listeners-to-other-elements
-
-        // ScTabs are contained by SlTabPanel components which gain the 'active'
-        // attribute when the respective tab is active. Therefore we observe
-        // changes on that SlTabPanel and when it becomes active, we know that
-        // this tab is visible.
-
-        // Bind the callback to 'this' instance so that it can access class properties.
-        const mutationCallback = this.checkVisible.bind(this)
-
-        // Options for the observer (which mutations to observe).
-        const config = { attributes: true }
-
-        this.observer = new MutationObserver(mutationCallback)
-        this.observer.observe(this.parentElement, config)
-    }
-
-    /**
-     * Removes the 'click' event handler in the case that the tab is destroyed so that the window
-     * does not attempt to trigger the handler when it is no longer accessible.
-     */
-    disconnectedCallback () {
-        super.disconnectedCallback()
-        this.observer.disconnect()
-    }
-
     /**
      * Provides the template for when the tab is visible (active).
      */
@@ -84,13 +27,7 @@ export class ScTab extends LitElement {
     }
 
     render () {
-        // Use 'cache()' to cache tabs which means their content will be saved after being loaded for
-        // the first time and does not need to be re-generated every time.
-        return html`
-        ${cache(this.visible
-            ? html`${this.visibleTemplate()}`
-            : html``
-    )}`
+        return this.visibleTemplate()
     }
 }
 
