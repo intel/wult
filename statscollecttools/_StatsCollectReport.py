2697b236646de435bb9bab6522d17c3d25efb95b
Adam Hawley <adam.james.hawley@intel.com>
2022-11-22 17:37:44 +0200

    statscollecttools: add '_StatsCollectReport'
    This patch is preparation for adding the 'stats-collect' tool with basic
    report generation functionality.
    
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

---
+++ statscollecttools/_StatsCollectReport.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# vim: ts=4 sw=4 tw=100 et ai si
+#
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Author: Adam Hawley <adam.james.hawley@intel.com>
+
+"""
+This module includes the "report" 'stats-collect' command implementation.
+"""
+
+from pepclibs.helperlibs import YAML
+from pepclibs.helperlibs.Exceptions import Error
+from statscollectlibs.htmlreport import HTMLReport
+
+def report_command(args):
+    """Implements the 'report' command."""
+
+    # Dictionary to reflecting 'info.yml' contents.
+    infos = {}
+
+    # Dictionary to reflecting 'datapoints.csv' contents.
+    stats_paths = {}
+
+    for respath in args.respaths:
+        info_path = respath / "info.yml"
+        if not info_path.is_file():
+            raise Error(f"unable to open info file '{info_path}'")
+        info_yml = YAML.load(info_path)
+
+        reportid = info_yml.get("reportid", f"report-{len(infos)}")
+        infos[reportid] = info_yml
+
+        stats_path = respath / "stats"
+        if not stats_path.is_dir():
+            raise Error(f"unable to find statistics directory '{stats_path}'")
+        stats_paths[reportid] = stats_path
+
+    if not args.outdir:
+        args.outdir = args.respaths[0] / "html-report"
+
+    rep = HTMLReport.HTMLReport(args.outdir)
+    rep.generate_report(stats_paths=stats_paths)
