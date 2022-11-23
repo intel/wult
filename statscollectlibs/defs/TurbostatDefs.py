intel/wult

commit dbf84f604ceefa605d689b9ef9adcddff92a4e69

TurbostatDefs: add helper functions mapping metrics to state names
This patch is in preparation for adding definitions for the "POLL"
requestable and hardware state. It will complicate some of the
transformations which have to be made to metric names and state names to
map between them. This mapping is better handled by being split into
simple helper functions.

Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

statscollectlibs/defs/TurbostatDefs.py
@@ -19,7 +19,18 @@ def is_reqcs_metric(metric):
     C-state.
     """
 
-    return metric.startswith("C") and metric[1].isdigit() and metric.endswith("%")
+    return metric == "POLL%" or (metric.startswith("C") and metric[1].isdigit() and
+                                 metric.endswith("%"))
+
+def get_reqcs_from_metric(metric):
+    """Returns the name of the requested C-state represented by 'metric'."""
+
+    return metric[:-1]
+
+def get_metric_from_reqcs(reqcs):
+    """Returns the metric which represents the requestable state 'reqcs'."""
+
+    return f"{reqcs}%"
 
 def is_hwcs_metric(metric):
     """
@@ -27,7 +38,17 @@ def is_hwcs_metric(metric):
     C-state.
     """
 
-    return metric.startswith("CPU%")
+    return metric.startswith("CPU%") or metric == "POLL"
+
+def get_hwcs_from_metric(metric):
+    """Returns the name of the hardware C-state represented by 'metric'."""
+
+    return metric[4:]
+
+def get_metric_from_hwcs(hwcs):
+    """Returns the metric which represents the hardware state 'hwcs'."""
+
+    return f"CPU%{hwcs.lower()}"
 
 def is_pkgcs_metric(metric):
     """
@@ -36,6 +57,16 @@ def is_pkgcs_metric(metric):
     """
 
     return metric.startswith("Pkg%")
+
+def get_pkgcs_from_metric(metric):
+    """Returns the name of the package C-state represented by 'metric'."""
+
+    return metric[5:]
+
+def get_metric_from_pkgcs(pkgcs):
+    """Returns the metric which represents the package state 'pkgcs'."""
+
+    return f"Pkg%{pkgcs.lower()}"
 
 class TurbostatDefs(_STCDefsBase.STCDefsBase):
     """This module provides API to turbostat metrics definitions (AKA 'defs')."""

statscollectlibs/htmlreport/tabs/turbostat/_TotalsL2TabBuilder.py
@@ -14,6 +14,7 @@ Please, refer to '_TurbostatL2TabBuilderBase' for more information about level 2
 
 import pandas
 from statscollectlibs.htmlreport.tabs.turbostat import _TurbostatL2TabBuilderBase
+from statscollectlibs.defs import TurbostatDefs
 
 class TotalsL2TabBuilder(_TurbostatL2TabBuilderBase.TurbostatL2TabBuilderBase):
     """
@@ -43,7 +44,7 @@ class TotalsL2TabBuilder(_TurbostatL2TabBuilderBase.TurbostatL2TabBuilderBase):
         # Add package C-states.
         hw_pkg_cs = self._get_common_elements(self._cstates["hardware"]["package"])
         for cs in hw_pkg_cs:
-            harchy["C-states"]["Hardware"]["dtabs"].append(f"Pkg%p{cs.lower()}")
+            harchy["C-states"]["Hardware"]["dtabs"].append(TurbostatDefs.get_metric_from_pkgcs(cs))
 
         return harchy
 

statscollectlibs/htmlreport/tabs/turbostat/_TurbostatL2TabBuilderBase.py
@@ -50,11 +50,11 @@ class TurbostatL2TabBuilderBase(_TabBuilderBase.TabBuilderBase):
 
         for metric in tstat["totals"]:
             if TurbostatDefs.is_reqcs_metric(metric):
-                req_cstates.append(metric[:-1])
+                req_cstates.append(TurbostatDefs.get_reqcs_from_metric(metric))
             elif TurbostatDefs.is_hwcs_metric(metric):
-                hw_cstates.append(metric[4:].upper())
+                hw_cstates.append(TurbostatDefs.get_hwcs_from_metric(metric))
             elif TurbostatDefs.is_pkgcs_metric(metric):
-                pkg_cstates.append(metric[5:].upper())
+                pkg_cstates.append(TurbostatDefs.get_pkgcs_from_metric(metric))
 
         self._cstates["hardware"]["core"].append(hw_cstates)
         self._cstates["hardware"]["package"].append(pkg_cstates)
@@ -145,10 +145,12 @@ class TurbostatL2TabBuilderBase(_TabBuilderBase.TabBuilderBase):
         req_cs = self._get_common_elements(self._cstates["requested"])
 
         for cs in req_cs:
-            tab_hierarchy["C-states"]["Requested"]["dtabs"].append(f"{cs}%")
+            metric = TurbostatDefs.get_metric_from_reqcs(cs)
+            tab_hierarchy["C-states"]["Requested"]["dtabs"].append(metric)
 
         for cs in hw_core_cs:
-            tab_hierarchy["C-states"]["Hardware"]["dtabs"].append(f"CPU%{cs.lower()}")
+            metric = TurbostatDefs.get_metric_from_hwcs(cs)
+            tab_hierarchy["C-states"]["Hardware"]["dtabs"].append(metric)
 
         return tab_hierarchy
 

commit c739c641e7bddf0e34722d5558531caac69c62f6

TurbostatDefs: fix 'get_metric_from_pkgcs()' helper
This function was wrongly returning metrics such as 'Pkg%c2' when the
actual metric should be 'Pkg%pc2'. This patch fixes this regression
which was introduced in commit:

dbf84f604ceefa605d689b9ef9adcddff92a4e69: TurbostatDefs: add helper
    functions mapping metrics to state names

Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

statscollectlibs/defs/TurbostatDefs.py
@@ -66,7 +66,7 @@ def get_pkgcs_from_metric(metric):
 def get_metric_from_pkgcs(pkgcs):
     """Returns the metric which represents the package state 'pkgcs'."""
 
-    return f"Pkg%{pkgcs.lower()}"
+    return f"Pkg%p{pkgcs.lower()}"
 
 class TurbostatDefs(_STCDefsBase.STCDefsBase):
     """This module provides API to turbostat metrics definitions (AKA 'defs')."""