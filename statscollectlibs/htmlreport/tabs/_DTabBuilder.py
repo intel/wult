ee7e394b4ae814302b931f19cbd8f3c847514bde
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2022-12-02 15:13:29 +0200

    _DTabBuilder: improve error messages and address pylint warnings
    Use the following pattern for error messages:
    
      raise Error("blah blah:\n{err.indent(2)}")
    
    instead of
    
      raise Error("blah blah: {err}")
    
    This new pattern results in more readable messages.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- statscollectlibs/htmlreport/tabs/_DTabBuilder.py
+++ statscollectlibs/htmlreport/tabs/_DTabBuilder.py
@@ -218,7 +218,8 @@ class DTabBuilder:
         try:
             self._outdir.mkdir(parents=True, exist_ok=True)
         except OSError as err:
-            raise Error(f"failed to create directory '{self._outdir}': {err}") from None
+            msg = Error(err).indent(2)
+            raise Error(f"failed to create directory '{self._outdir}':\n{msg}") from None
 
         # Paths of plots generated for this tab.
         self._ppaths = []
