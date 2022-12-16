84680e83c0997be78ba43c23ec2e24173851835d
Adam Hawley <adam.james.hawley@intel.com>
2022-12-16 13:16:52 +0200

    statscollecttools: add '_StatsCollectStart'
    This patch adds the '_StatsCollectStart.start_command()' function which
    contains the process for running 'stats-collect start' which will be
    added in a following patch.
    
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

---
+++ statscollecttools/_StatsCollectStart.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+# vim: ts=4 sw=4 tw=100 et ai si
+#
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Authors: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
+#          Adam Hawley <adam.james.hawley@intel.com>
+
+"""This module includes the "start" 'stats-collect' command implementation."""
+
+import contextlib
+import time
+from pathlib import Path
+from pepclibs import CPUInfo
+from pepclibs.helperlibs import Human, Logging
+from pepclibs.helperlibs.Exceptions import Error
+from statscollectlibs import ToolsCommon
+from statscollectlibs.collector import STCHelpers
+from statscollectlibs.helperlibs import ReportID
+from statscollectlibs.rawresultlibs import RawResult
+
+def generate_reportid(args, pman):
+    """
+    If user provided report ID for the 'start' command, this function validates it and returns.
+    Otherwise, it generates the default report ID and returns it.
+    """
+
+    if not args.reportid and pman.is_remote:
+        prefix = pman.hostname
+    else:
+        prefix = None
+
+    return ReportID.format_reportid(prefix=prefix, reportid=args.reportid,
+                                    strftime=f"{args.toolname}-%Y%m%d")
+
+def _generate_report(res, outdir):
+    """Implements the 'report' command for start."""
+
+    from statscollectlibs.htmlreport import HTMLReport # pylint: disable=import-outside-toplevel
+
+    stats_paths =  {res.reportid: res.stats_path}
+    rep = HTMLReport.HTMLReport(outdir)
+    rep.generate_report(stats_paths=stats_paths)
+
+def start_command(args):
+    """Implements the 'start' command."""
+
+    with contextlib.ExitStack() as stack:
+        pman = ToolsCommon.get_pman(args)
+        stack.enter_context(pman)
+
+        args.tlimit = Human.parse_duration(args.tlimit, default_unit="m", name="time limit")
+        args.reportid = generate_reportid(args, pman)
+
+        if not args.outdir:
+            args.outdir = Path(f"./{args.reportid}")
+
+        cpuinfo = CPUInfo.CPUInfo(pman=pman)
+        stack.enter_context(cpuinfo)
+        args.cpunum = cpuinfo.normalize_cpu(args.cpunum)
+
+        res = RawResult.RawResult(args.reportid, args.outdir, args.toolver, args.cpunum)
+
+        Logging.setup_stdout_logging(args.toolname, res.logs_path)
+
+        if not args.stats:
+            raise Error("No statistics specified. Use '--stats' to specify which statistics "
+                        "should be collected.")
+        stcoll = STCHelpers.create_and_configure_stcoll(args.stats, args.stats_intervals,
+                                                        args.outdir, pman, args.toolname)
+        if not stcoll:
+            return
+
+        stack.enter_context(stcoll)
+
+        stcoll.start()
+        time.sleep(args.tlimit)
+        stcoll.stop()
+        stcoll.copy_remote_data()
+
+    if args.report:
+        _generate_report(res, args.outdir)
