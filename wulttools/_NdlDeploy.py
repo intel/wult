92e58c22bacfdd22f593e87ce67af187ae7f5ee2
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2022-12-27 19:56:08 +0200

    ndl: deploy stc-agent on 'ndl deploy'
    Make 'ndl deploy' also deploy the 'stc-agent' helper. This is necessary because
    now we collect statistics with 'ndl' tool too.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- wulttools/_NdlDeploy.py
+++ wulttools/_NdlDeploy.py
@@ -10,11 +10,22 @@
 This module includes the "deploy" 'ndl' command implementation.
 """
 
+import logging
+from pepclibs.helperlibs.Exceptions import Error
 from wulttools import _Common
 from wultlibs.deploylibs import Deploy
 
+_LOG = logging.getLogger()
+
 def deploy_command(args):
     """Implements the 'deploy' command."""
+
+    try:
+        _Common.run_stats_collect_deploy(args)
+    except Error as err:
+        _LOG.warning(err)
+        _LOG.notice(f"the statistics collection capability will not be available for "
+                    f"'{args.toolname}'")
 
     with _Common.get_pman(args) as pman, \
          Deploy.Deploy(args.toolname, args.deploy_info, pman=pman, ksrc=args.ksrc,
