afce8506056b0612e346d149949517499d078844
Adam Hawley <adam.james.hawley@intel.com>
2022-12-29 14:29:08 +0200

    STCHelpers: refresh docstring
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

--- statscollectlibs/collector/STCHelpers.py
+++ statscollectlibs/collector/STCHelpers.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 # vim: ts=4 sw=4 tw=100 et ai si
 #
-# Copyright (C) 2019-2021 Intel Corporation
+# Copyright (C) 2019-2022 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # Author: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
 
-"""
-This module implements several misc. helpers for tools using 'StatsCollect'.
-"""
+"""This module provides the API for building an instance of 'StatsCollect'."""
 
 import contextlib
 import logging
