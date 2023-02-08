5d195f627f0290e15630ca94e38e8efb0ac592cf
Antti Laakso <antti.laakso@intel.com>
2023-02-08 15:45:57 +0200

    exercise-sut: increase version number
    Many bugfixes and improvements recently, update
    version number.
    
    Signed-off-by: Antti Laakso <antti.laakso@intel.com>

--- wulttools/_ExerciseSut.py
+++ wulttools/_ExerciseSut.py
@@ -25,7 +25,7 @@ from pepclibs.helperlibs.Exceptions import Error
 from wulttools import _BatchConfig
 
 _OWN_NAME = "exercise-sut"
-_VERSION = "1.1.0"
+_VERSION = "1.1.1"
 
 Logging.setup_logger(prefix=_OWN_NAME)
 _LOG = logging.getLogger()
