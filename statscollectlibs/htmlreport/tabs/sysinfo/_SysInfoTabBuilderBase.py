0b748f660fe8c368b4faec6244dc0dea02391560
Adam Hawley <adam.james.hawley@intel.com>
2023-02-23 15:02:02 +0200

    _SysInfoTabBuilderBase: change the class to inherit from 'DTabBuilder'
    The 'DTabBuilder' class is the generalised class for building data tabs.
    In a recent patch, it was adjusted to include the ability to add a file
    previews. 'SysInfoTabBuilderBase' already uses 'DTabBuilder' for this
    functionality. This patch clarifies the relationship between the two
    classes and makes the interfaces more consistent.
    
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

--- statscollectlibs/htmlreport/tabs/sysinfo/_SysInfoTabBuilderBase.py
+++ statscollectlibs/htmlreport/tabs/sysinfo/_SysInfoTabBuilderBase.py
@@ -12,13 +12,10 @@ This module provides the capability of populating a "SysInfo" data tab.
 "SysInfo" tabs contain various system information about the systems under test (SUTs).
 """
 
-import logging
 from pepclibs.helperlibs.Exceptions import Error
 from statscollectlibs.htmlreport.tabs import _DTabBuilder
 
-_LOG = logging.getLogger()
-
-class SysInfoTabBuilderBase:
+class SysInfoTabBuilderBase(_DTabBuilder.DTabBuilder):
     """
     This base class provides the capability of populating a "SysInfo" tab.
 
@@ -29,21 +26,10 @@ class SysInfoTabBuilderBase:
     def get_tab(self):
         """Returns a '_Tabs.DTabDC' instance which represents system information."""
 
-        if any(not fp for fp in self.stats_paths.values()):
-            raise Error("unable to add file previews since not all reports have a statistics dir.")
+        self.add_fpreviews(self.stats_paths, self.files)
 
-        mdef = {
-            "name": self.name,
-            "fsname": self.name,
-            "title": self.name,
-            "descr": self.name
-        }
-
-        dtabbldr = _DTabBuilder.DTabBuilder({}, self.outdir, mdef, self.outdir)
-        dtabbldr.add_fpreviews(self.stats_paths, self.files)
-
-        if dtabbldr.fpreviews:
-            return dtabbldr.get_tab()
+        if self.fpreviews:
+            return super().get_tab()
 
         raise Error(f"unable to build '{self.name}' SysInfo tab, no file previews could be "
                     f"generated.")
@@ -61,7 +47,19 @@ class SysInfoTabBuilderBase:
                          path to the directory which contains raw statistics files for 'ReportID'.
         """
 
+        if any(not fp for fp in stats_paths.values()):
+            raise Error("Unable to add file previews since not all reports have a statistics dir.")
+
+        # All of the names of 'SysInfo' tabs are file-safe and are also used as the title of the
+        # tab.
+        mdef = {
+            "name": name,
+            "fsname": name,
+            "title": name
+        }
+
+        super().__init__({}, outdir, mdef, outdir)
+
         self.name = name
-        self.outdir = outdir
         self.files = files
         self.stats_paths = stats_paths
