cc2b21551cb33066fbcb4297a6b8423e98c219a1
Adam Hawley <adam.james.hawley@intel.com>
2023-02-28 13:22:28 +0200

    _SpecStatsCollect: fix 'FileNotFoundError' in 'stats-collect start'
    Fix the following error by creating the statistics output directory
    before dumping the statistics info yaml file.
    
    FileNotFoundError: [Errno 2] No such file or directory: 'outdir/stats/stats-info.yml'
    
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

--- statscollectlibs/collector/_SpecStatsCollect.py
+++ statscollectlibs/collector/_SpecStatsCollect.py
@@ -369,6 +369,10 @@ class SpecStatsCollect(ClassHelpers.SimpleCloseContext):
                 yml[stname] = info
 
         path = self.local_outdir / "stats" / "stats-info.yml"
+
+        # Ensure that "stats" directory exists and create it if it doesn't.
+        path.parent.mkdir(parents=True, exist_ok=True)
+
         YAML.dump(yml, path)
 
     def stop(self, sysinfo=True):
