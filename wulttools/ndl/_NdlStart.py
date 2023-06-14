edcb3afd888778934ce26f3109970bef03a6cf60
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2023-06-14 20:25:11 +0300

    _WultStart.py: pass 'cpunum' as kwarg
    As a convention, in this project we pass keyword arguments using the
    'name=value' syntax.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- wulttools/ndl/_NdlStart.py
+++ wulttools/ndl/_NdlStart.py
@@ -90,7 +90,8 @@ def start_command(args):
             args.cpunum = _resolve_cpu(pman, args.devid)
 
         args.cpunum = cpuinfo.normalize_cpu(args.cpunum)
-        res = WORawResult.WORawResult("ndl", args.toolver, args.reportid, args.outdir, args.cpunum)
+        res = WORawResult.WORawResult("ndl", args.toolver, args.reportid, args.outdir,
+                                      cpunum=args.cpunum)
         stack.enter_context(res)
 
         Logging.setup_stdout_logging(args.toolname, res.logs_path)
