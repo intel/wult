87f63bc075a9180520a0e3ae088210142a8a15d7
Niklas Neronin <niklas.neronin@intel.com>
2023-07-27 09:56:54 +0300

    _BatchConfig: optimize '_normalize_pcsnames()'
    Change 'allpcsnames' and 'pcsaliases' from list to set. Duplicate values
    can not be added to a set, which makes it faster than searching a list for
    duplicate value, before adding.
    
    Uppercase all values in 'pcsnames' once, instead of 2-3 times.
    
    Signed-off-by: Niklas Neronin <niklas.neronin@intel.com>

--- wulttools/exercisesut/_BatchConfig.py
+++ wulttools/exercisesut/_BatchConfig.py
@@ -171,28 +171,28 @@ class _PropIteratorBase(ClassHelpers.SimpleCloseContext):
     def _normalize_pcsnames(self, pcsnames):
         """Normalize and validate list of package C-state names 'pcsnames'."""
 
-        allpcsnames = []
-        pcsaliases = []
+        allpcsnames = set()
+        pcsaliases = set()
         cstates = self._get_cstates()
         for _, pinfo in cstates.get_props(("pkg_cstate_limits", "pkg_cstate_limit_aliases")):
             if pinfo["pkg_cstate_limits"]:
                 for pcsname in pinfo["pkg_cstate_limits"]:
-                    if pcsname not in allpcsnames:
-                        allpcsnames.append(pcsname)
+                    allpcsnames.add(pcsname)
 
             if pinfo["pkg_cstate_limit_aliases"]:
                 for pcsalias in pinfo["pkg_cstate_limit_aliases"]:
-                    if pcsalias not in pcsaliases:
-                        pcsaliases.append(pcsalias)
+                    pcsaliases.add(pcsalias)
 
         if "all" in pcsnames:
-            return allpcsnames
+            return list(allpcsnames)
+
+        pcsnames = [pcsname.upper() for pcsname in pcsnames]
 
         for pcsname in pcsnames:
-            if pcsname.upper() not in allpcsnames and pcsname.upper() not in pcsaliases:
+            if pcsname not in allpcsnames and pcsname not in pcsaliases:
                 raise Error(f"package C-state '{pcsname}' not available{self._pman.hostmsg}")
 
-        return [pcsname.upper() for pcsname in pcsnames]
+        return pcsnames
 
     def _normalize_csnames(self, csnames):
         """Normalize and validate list of requestable C-state names 'csnames'."""
