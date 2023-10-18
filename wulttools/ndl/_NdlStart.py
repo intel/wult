7bc68e110265e473c8b4dd250534ad093a3b4231
Antti Laakso <antti.laakso@intel.com>
2023-10-18 15:30:23 +0300

    _NdlStart: fix _check_settings()
    Fix _check_settings() by adding missing colon, and
    defining correct property name.
    
    Signed-off-by: Antti Laakso <antti.laakso@intel.com>

--- wulttools/ndl/_NdlStart.py
+++ wulttools/ndl/_NdlStart.py
@@ -112,7 +112,7 @@ def _check_settings(pman, cpuinfo, cpunum):
          CPUIdle.CPUIdle(pman=pman, cpuinfo=cpuinfo) as cpuidle, \
          CStates.CStates(pman=pman, cpuinfo=cpuinfo, cpuidle=cpuidle) as cstates:
 
-        if cstates.get_cpu_prop(pname, cpunum) == "PC0":
+        if cstates.get_cpu_prop("pkg_cstate_limit", cpunum) == "PC0":
             return
 
         csinfo = cpuidle.get_cpu_cstates_info(cpunum)
@@ -123,7 +123,7 @@ def _check_settings(pman, cpuinfo, cpunum):
             return
 
         for pname in ("c1_demotion", "cstate_prewake"):
-            if cstates.get_cpu_prop(pname, cpunum) == "on"
+            if cstates.get_cpu_prop(pname, cpunum) == "on":
                 name = cstates.props[pname]["name"]
                 _LOG.notice("%s is enabled, this may lead to lower C6 residency. It is " \
                             "recommended to disable %s.", name, name)
