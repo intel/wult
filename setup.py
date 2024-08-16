7b90f9fd1ef394ea91a46be7ad91a22ac0541d9a
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2024-08-16 10:54:21 +0300

    setup.py: bump pepc and stats-collect version requirements
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
                get_data_files("share/wult/defs/wult", "defs/wult"),
     scripts=_TOOLNAMES + _PYTHON_HELPERS,
     packages=find_packages(),
-    install_requires=["pepc>=1.5.15", "stats-collect>=1.0.25", "numpy", "pandas", "pyyaml",
+    install_requires=["pepc>=1.5.18", "stats-collect>=1.0.29", "numpy", "pandas", "pyyaml",
                       "colorama"],
     long_description="""This package provides wult - a Linux command-line tool for measuring Intel
                         CPU C-state wake latency.""",
