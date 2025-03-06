525c553f3030d0fdf0dc0192703ebaddb70b3dbd
Antti Laakso <antti.laakso@intel.com>
2025-03-06 17:25:19 +0200

    wulttools: adjust to StatsCollectBuilder changes
    The 'cpu' argument was renamed to 'cpus'.
    
    Signed-off-by: Antti Laakso <antti.laakso@intel.com>

--- wulttools/pbe/_PbeStart.py
+++ wulttools/pbe/_PbeStart.py
@@ -90,7 +90,7 @@ def start_command(args):
         if args.stats_intervals:
             stcoll_builder.parse_intervals(args.stats_intervals)
 
-        stcoll = stcoll_builder.build_stcoll_nores(pman, args.reportid, cpu=(args.lead_cpu,),
+        stcoll = stcoll_builder.build_stcoll_nores(pman, args.reportid, cpus=(args.lead_cpu,),
                                                    local_outdir=res.stats_path)
         if stcoll:
             stack.enter_context(stcoll)
